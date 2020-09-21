63291a02e2e46b70b2b35d9eac4dfb3fdbaa599c
technoir <sergey.chelombitko@gmail.com>
2020-09-21 22:04:14 +0100

    Minor refactoring

--- plugin/src/main/kotlin/com/github/technoir42/plugin/aarpublish/AarPublishPlugin.kt
+++ plugin/src/main/kotlin/com/github/technoir42/plugin/aarpublish/AarPublishPlugin.kt
@@ -1,7 +1,9 @@
 package com.github.technoir42.plugin.aarpublish
 
+import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.api.AndroidSourceSet
+import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.api.LibraryVariant
 import com.android.build.gradle.api.SourceKind
 import com.android.utils.appendCapitalized
@@ -16,6 +18,7 @@ import org.gradle.api.component.ConfigurationVariantDetails
 import org.gradle.api.component.SoftwareComponentFactory
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.external.javadoc.CoreJavadocOptions
@@ -40,7 +43,6 @@ class AarPublishPlugin @Inject constructor(
 
         libraryExtension.libraryVariants.all { variant ->
             val archives = createArchivesConfigurationForVariant(project, variant, libraryExtension, aarPublishingExtension)
-
             val component = softwareComponentFactory.adhoc("android".appendCapitalized(variant.name))
             val apiElements = project.configurations.getByName("${variant.name}ApiElements")
             val runtimeElements = project.configurations.getByName("${variant.name}RuntimeElements")
@@ -53,54 +55,65 @@ class AarPublishPlugin @Inject constructor(
         }
     }
 
-    private fun createArchivesConfigurationForVariant(project: Project, variant: LibraryVariant, libraryExtension: LibraryExtension, aarPublishingExtension: AarPublishingExtension): Configuration {
+    private fun createArchivesConfigurationForVariant(
+        project: Project,
+        variant: LibraryVariant,
+        baseExtension: BaseExtension,
+        aarPublishingExtension: AarPublishingExtension
+    ): Configuration {
         val archives = project.configurations.create("${variant.name}Archives")
         project.artifacts.add(archives.name, variant.packageLibraryProvider)
 
         if (aarPublishingExtension.publishJavadoc) {
-            val javadoc = project.tasks.register("javadoc${variant.name.capitalize()}", Javadoc::class.java) { task ->
-                task.group = JavaBasePlugin.DOCUMENTATION_GROUP
-                task.description = "Generates Javadoc API documentation for ${variant.name} variant."
-                task.dependsOn(variant.javaCompileProvider)
-                variant.getSourceFolders(SourceKind.JAVA).forEach { task.source += it }
-                task.classpath += project.files(libraryExtension.bootClasspath)
-                task.classpath += variant.javaCompileProvider.get().classpath
-                task.destinationDir = File(project.buildDir, "docs/javadoc/${variant.name}")
-                if (JavaVersion.current().isJava9Compatible) {
-                    (task.options as CoreJavadocOptions).addBooleanOption("html5", true)
-                }
-            }
-
-            val javadocJar = project.tasks.register("package${variant.name.capitalize()}Javadoc", Jar::class.java) { task ->
-                task.group = BasePlugin.BUILD_GROUP
-                task.description = "Assembles a jar archive containing the javadoc of ${variant.name} variant."
-                task.dependsOn(javadoc)
-                task.from(javadoc.get().destinationDir)
-                task.archiveClassifier.set("javadoc")
-            }
-
+            val javadocJar = registerJavadocTasks(project, variant, baseExtension)
             project.artifacts.add(archives.name, javadocJar)
         }
 
         if (aarPublishingExtension.publishSources) {
-            val sourcesJar = project.tasks.register("package${variant.name.capitalize()}Sources", Jar::class.java) { task ->
-                task.group = BasePlugin.BUILD_GROUP
-                task.description = "Assembles a jar archive containing the sources of ${variant.name} variant."
-                task.from(variant.getSourceFolders(SourceKind.JAVA))
-
-                val kotlinSources = variant.sourceSets.asSequence()
-                    .filterIsInstance<AndroidSourceSet>()
-                    .flatMap { it.java.sourceDirectoryTrees.asSequence() }
-                    .map { it.setIncludes(setOf("**/*.kt")) }
-                    .toList()
-
-                task.from(kotlinSources)
-                task.archiveClassifier.set("sources")
-            }
-
+            val sourcesJar = registerSourcesTask(project, variant)
             project.artifacts.add(archives.name, sourcesJar)
         }
         return archives
+    }
+
+    private fun registerJavadocTasks(project: Project, variant: BaseVariant, baseExtension: BaseExtension): TaskProvider<Jar> {
+        val javadoc = project.tasks.register("javadoc${variant.name.capitalize()}", Javadoc::class.java) { task ->
+            task.group = JavaBasePlugin.DOCUMENTATION_GROUP
+            task.description = "Generates Javadoc API documentation for ${variant.name} variant."
+            task.dependsOn(variant.javaCompileProvider)
+            variant.getSourceFolders(SourceKind.JAVA).forEach { task.source += it }
+            task.classpath += project.files(baseExtension.bootClasspath)
+            task.classpath += variant.javaCompileProvider.get().classpath
+            task.destinationDir = File(project.buildDir, "docs/javadoc/${variant.name}")
+            if (JavaVersion.current().isJava9Compatible) {
+                (task.options as CoreJavadocOptions).addBooleanOption("html5", true)
+            }
+        }
+
+        return project.tasks.register("package${variant.name.capitalize()}Javadoc", Jar::class.java) { task ->
+            task.group = BasePlugin.BUILD_GROUP
+            task.description = "Assembles a jar archive containing the javadoc of ${variant.name} variant."
+            task.dependsOn(javadoc)
+            task.from(javadoc.get().destinationDir)
+            task.archiveClassifier.set("javadoc")
+        }
+    }
+
+    private fun registerSourcesTask(project: Project, variant: BaseVariant): TaskProvider<Jar> {
+        return project.tasks.register("package${variant.name.capitalize()}Sources", Jar::class.java) { task ->
+            task.group = BasePlugin.BUILD_GROUP
+            task.description = "Assembles a jar archive containing the sources of ${variant.name} variant."
+            task.from(variant.getSourceFolders(SourceKind.JAVA))
+
+            val kotlinSources = variant.sourceSets.asSequence()
+                .filterIsInstance<AndroidSourceSet>()
+                .flatMap { it.java.sourceDirectoryTrees.asSequence() }
+                .map { it.setIncludes(setOf("**/*.kt")) }
+                .toList()
+
+            task.from(kotlinSources)
+            task.archiveClassifier.set("sources")
+        }
     }
 
     private fun AdhocComponentWithVariants.setupFromConfigurations(archives: Configuration, apiElements: Configuration, runtimeElements: Configuration) {
